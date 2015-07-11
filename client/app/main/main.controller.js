830e20921ca6c794f669226887cbe08814326291
A. Drake <a.l.drake713@gmail.com>
2015-07-11 10:22:25 -0400

    Added animation to main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,14 +1,22 @@
 'use strict';
 
 angular.module('pinterestApp')
-  .controller('MainCtrl', ['$scope', '$http', 'socket', 'WinsProvider', 'Auth', function ($scope, $http, socket, Wins, Auth) {
+  .controller('MainCtrl', ['$scope', '$http', 'socket', 'WinsProvider', 'Auth', '$interval', 
+              function ($scope, $http, socket, Wins, Auth, $interval) {
     $scope.wins = [];
-    $scope.testWins = [];
+    $scope.queue = [];
     $scope.isLoggedIn = Auth.isLoggedIn;
     
     
     $http.get('/api/wins/recent').success(function(recentWins) {
-      $scope.wins = recentWins.slice(0, 5);
+      $scope.wins = recentWins.slice(0,2);
+      $scope.queue = recentWins.slice(2, 5);
+      
+      console.log($scope.queue, $scope.wins);
+
+      // a timeout for the nifty animation - gets the reader's attention
+      var popWins = $interval($scope.popNewWin, 1500, 3);
+
       socket.syncUpdates('win', $scope.wins, function(event, win, wins) {
         if ($scope.wins.length > 5) {
           $scope.wins.shift();
@@ -19,5 +27,20 @@ angular.module('pinterestApp')
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('win');
+      $scope.stopTimer();
+
     });
+
+    // helper function for the animation
+    $scope.popNewWin = function popNewWin() {
+      $scope.wins.unshift($scope.queue.pop());
+    }
+
+    // helper fxn to destroy the timer
+    $scope.stopTimer = function killTimer() {
+      if (angular.isDefined(popWins)) {
+        $interval.cancel(popWins);
+        popWins = undefined;
+      }
+    }
   }]);
